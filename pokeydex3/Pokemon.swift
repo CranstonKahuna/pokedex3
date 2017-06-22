79bbce0e61cc4b58cb831069cdc9e926f0d1257e
Dean Thomas <deant@dean-thomass-macbook-pro.local>
2017-06-22 14:27:00 -0700

    After lecture 87 - Create Pokemon model and cleanup ViewController

---
+++ pokeydex3/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokeydex3
+//
+//  Created by Dean Thomas on 6/22/17.
+//  Copyright © 2017 Dean Thomas. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
